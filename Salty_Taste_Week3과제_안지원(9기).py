f33e3ba75078330881a0add80822a56e018744b8
Ahn jiwon <jiwon643@naver.com>
2020-07-31 23:00:02 +0900

    Week3 과제(안지원 9기)-MongoDB 연결본
    MongoDB에 데이터 저장한 최종본입니다.

--- Salty_Taste_Week3과제_안지원(9기).py
+++ Salty_Taste_Week3과제_안지원(9기).py
@@ -1,9 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta
 
-import requests
-from bs4 import BeautifulSoup
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -12,10 +13,16 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 soup = BeautifulSoup(data.text, 'html.parser')
 
 Plays=soup.select("#body-content>div.newest-list>div>table>tbody>tr")
-rank=0
+
 # print(len(Plays))
 for Play in Plays:
-    rank+=1
+    Rank =Play.select_one('td.number').text[0:2].strip()
     Title=Play.select_one("td.info>a").text.strip()
     Artist=Play.select_one("td.info>a.artist.ellipsis").text
-    print(rank,Title,Artist)
+    print(Rank,Title,Artist)
+    doc = {
+        'Rank': Rank,
+        'Title': Title,
+        'Artist': Artist  # DB에는 숫자처럼 생긴 문자열 형태로 저장됩니다.
+    }
+    db.Plays.insert_one(doc)
